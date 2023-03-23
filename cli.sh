859f7081806e334a531426f374b240a46137539c
threewebcode <magestore@outlook.com>
2023-03-23 20:31:26 +0000

    feat: move contract

--- cli.sh
+++ cli.sh
@@ -4,7 +4,10 @@ function install(){
   git clone https://github.com/move-language/move.git
   cd move
   docker build -t move/cli -f docker/move-cli/Dockerfile .
-  docker run -v `pwd`:/project move/cli build
+}
+
+function move(){
+  docker run --rm -it -v $PWD:/project move/cli $@
 }
 
 $@
