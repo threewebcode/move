e261476febed62dc58f45feb0267663be51482e2
wonderfan <276659702@qq.com>
2023-05-13 01:23:13 +0000

    feat: proof system

--- proof/index.js
+++ proof/index.js
@@ -1,12 +1,27 @@
 const { MerkleTree } = require('merkletreejs');
 const SHA256 = require('crypto-js/sha256');
+const fs = require("fs/promises");
 
-const leaves = ['a', 'b', 'c'].map(x => SHA256(x));
-const tree = new MerkleTree(leaves, SHA256);
-const root = tree.getRoot().toString('hex');
-console.log(root);
-const leaf = SHA256('a');
-const proof = tree.getProof(leaf);
-console.log(leaf.toString());
-console.log(tree.verify(proof, leaf, root));
-console.log(tree.toString());
+const demo = async () => {
+    const leaves = ['a', 'b', 'c'].map(x => SHA256(x));
+    const tree = new MerkleTree(leaves, SHA256);
+    const root = tree.getRoot().toString('hex');
+    console.log(root);
+    const leaf = SHA256('a');
+    const proof = tree.getProof(leaf);
+    console.log(leaf.toString());
+    console.log(tree.verify(proof, leaf, root));
+    console.log(tree.toString());
+}
+
+const test = async () => {
+    let data = await fs.readFile("data/list.json").then((rawData) => JSON.parse(rawData));
+    console.log(data);
+}
+
+test();
+
+
+
+
+
