4203beb2971c69be3f11bd6d6542f786f3d37601
wonderfan <276659702@qq.com>
2023-05-13 02:38:41 +0000

    feat: proof system

---
+++ lang/scripts/crypto.js
@@ -0,0 +1,11 @@
+const { Ed25519Keypair } = require("@mysten/sui.js");
+const { fromB64 } = require('@mysten/bcs');
+
+const mnemonics = "stereo cattle target shoulder orbit tiny clown twin input phone green lake";
+const keypair = Ed25519Keypair.deriveKeypair(mnemonics);
+console.log(keypair.getPublicKey().toSuiAddress());
+let privateKey = "0x" + Buffer.from(fromB64(keypair.export().privateKey)).toString("hex");
+console.log(privateKey);
+let kp = Ed25519Keypair.fromSecretKey(Buffer.from(privateKey.slice(2), "hex"), { skipValidation: true });
+console.log("sui address: " + kp.getPublicKey().toSuiAddress());
+
