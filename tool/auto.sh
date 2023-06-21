1e2e3664d37d52911eab3d7fdb09845e39749bae
W3BUILD <127061691+threewebcode@users.noreply.github.com>
2023-06-21 17:58:21 +0800

    Update auto.sh

--- tool/auto.sh
+++ tool/auto.sh
@@ -9,5 +9,5 @@ while read line; do
   amounts+=(${fields[1]})
 done < "$csv_file"
 recipients_para=$(printf "%s " "${recipients[@]}")
-account_para=$(printf "%d " "${amounts[@]}")
-sui client pay-sui --input-coins "0x1fd49faed488d030dcea1463aee14c819fd0bb5c5e44d563ea33c7405fae4e24" --recipients ${recipients_para} --amounts ${account_para} --gas-budget 1000000000
+amounts_para=$(printf "%d " "${amounts[@]}")
+sui client pay-sui --input-coins "0x1fd49faed488d030dcea1463aee14c819fd0bb5c5e44d563ea33c7405fae4e24" --recipients ${recipients_para} --amounts ${amounts_para} --gas-budget 1000000000
